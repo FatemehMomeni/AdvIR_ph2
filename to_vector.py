d278f46e5c83b24aa2611216426a1daff7e88ae3
FatemehMomeni <50900356+FatemehMomeni@users.noreply.github.com>
2022-05-06 18:02:14 +0430

    converting lecture's transcripts to vector

--- to_vector.py
+++ to_vector.py
@@ -1,12 +1,10 @@
-import fastText
 from elasticsearch import Elasticsearch
 from fastText.python.fasttext_module import fasttext
 
 ELASTIC_PASSWORD = "vbEP_fOLmUvGy==16gvZ"
 es = Elasticsearch('https://localhost:9200', verify_certs=False, basic_auth=("elastic", ELASTIC_PASSWORD))
 
-
-for i in range(3):
+for i in range(5440):
     resp = es.get(index='ted_index', id=i)
     with open('ted_index.doc'+str(i)+'.txt', 'w', encoding='utf-8') as f:
         f.write(resp['_source']['transcript'])
